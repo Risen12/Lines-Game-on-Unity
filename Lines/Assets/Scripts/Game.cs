34fcd9bd46c69ab68f8b043947510ac2f99b399f
Daniil Lautenshleger <alximik683@gmail.com>
2019-11-06 14:47:31 +0300

    first commit

---
+++ Lines/Assets/Scripts/Game.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour
+{
+    public GameObject object1 = new GameObject();
+    public AudioSource source;
+    public Text text;
+    [HideInInspector]
+    public AudioClip clip;
+
+
+    Button[,] buttons;
+    Image[] images;
+    Lines lines;
+
+    void Start()
+    {
+        object1.SetActive(false);
+        lines = new Lines(ShowBox, PlayCut, PlayEnd, PlayStart);
+        InitButtons();
+        InitImages();
+        ShowBox(1, 2, 3);
+        lines.Start();
+    }
+    public void ShowBox(int x, int y, int ball)
+    {
+        buttons[x, y].GetComponent<Image>().sprite = images[ball].sprite;
+    }
+    public void PlayCut()
+    {
+        source = source.GetComponent<AudioSource>();
+        clip = Resources.Load("cut", typeof(AudioClip)) as AudioClip;
+        source.clip = clip;
+        source.Play();
+        text.text = lines.Record.ToString();
+    }
+     
+    public void PlayStart()
+    {
+        source = source.GetComponent<AudioSource>();
+        clip = Resources.Load("startgame", typeof(AudioClip)) as AudioClip;
+        source.clip = clip;
+        source.Play();
+
+    } 
+    public void PlayEnd()
+    {
+        source = source.GetComponent<AudioSource>();
+        clip = Resources.Load("endgame", typeof(AudioClip)) as AudioClip;
+        source.clip = clip;
+        source.Play();
+    }
+    public void Click()
+    {
+        string name = EventSystem.current.currentSelectedGameObject.name;
+        int nr = GetNmber(name);
+        int x = nr % Lines.size;
+        int y = nr / Lines.size;
+        lines.Click(x,y);
+        if (lines.IsGameOver)
+        {
+            object1.SetActive(true);
+
+        }
+    }
+    private void InitButtons()
+    {
+        buttons = new Button[Lines.size, Lines.size];
+        for (int nr = 0; nr < Lines.size * Lines.size; nr++)
+            buttons[nr % Lines.size, nr / Lines.size] = GameObject.Find($"Button ({nr})").GetComponent<Button>();
+    }
+    private int GetNmber(string name)
+    {
+        Regex regex = new Regex("\\((\\d+)\\)");
+        Match math = regex.Match(name);
+        if (!math.Success)
+            throw new System.Exception();
+        Group group = math.Groups[1];
+        return Convert.ToInt32(group.Value);
+    }
+
+    private void InitImages()
+    {
+        images = new Image[Lines.balls];
+        for (int j = 0; j < Lines.balls; j++)
+            images[j] = GameObject.Find($"Image ({j})").GetComponent<Image>();
+    }
+
+}
---
+++ Lines/Assets/Scripts/Lines.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public delegate void ShowBox(int x, int y, int ball);
+public delegate void  PlayCut();
+public delegate void PlayStart();
+public delegate void  PlayEnd();
+public class Lines 
+{
+    public bool IsGameOver = false;
+    public int Record;
+    System.Random rand = new System.Random();
+    public const int size = 9;
+    public const int balls = 7;
+    public const int add_balls = 3;
+    public ShowBox Show_Box;
+    public PlayCut Play_Cut;
+    public PlayEnd Play_End;
+    public PlayStart Play_Start;
+
+    int fromX, fromY;
+    bool isbooltaked = false;
+
+    int[,] map;
+
+    public Lines(ShowBox showBox, PlayCut playCut, PlayEnd playEnd, PlayStart playStart)
+    {
+        Record = 0;
+        this.Show_Box = showBox;
+        this.Play_Cut = playCut;
+        this.Play_End = playEnd;
+        this.Play_Start = playStart;
+        map = new int[size, size];
+    }
+    public void Start()
+    {
+        clearMap();
+        Play_Start();
+        AddRandomBalls();
+        
+    }
+
+    private void AddRandomBalls()
+    {
+        for (int i = 0; i < add_balls; i++)
+            AddRandomBall();
+    }
+
+    private void AddRandomBall()
+    {
+        int x, y;
+        int loop = size * size;
+        do
+        {
+            x = rand.Next(size);
+            y = rand.Next(size);
+            if (--loop <= 0)
+            { 
+                
+            }
+        } while (map[x, y] > 0);
+        int ball = rand.Next(1, balls - 1);
+        SetMap(x, y, ball);
+    }
+
+    private void clearMap()
+    {
+        for(int i = 0; i < size; i++)
+            for(int j = 0; j < size; j++)
+                SetMap(i,j,0);
+    }
+
+    private void SetMap(int x, int y, int ball)
+    {
+        map[x, y] = ball;
+        Show_Box(x, y, ball);
+    }
+
+    public void Click(int x, int y)
+    {
+        if (map[x, y] > 0)
+        {
+            TakeBall(x, y);
+        }
+        else
+        {
+            MoveBall(x, y);
+            
+        }
+
+    }
+
+    private void MoveBall(int x, int y)
+    {
+        if (!isbooltaked) return;
+        if (!CanMove(x,y)) return;
+        SetMap(x, y, map[fromX, fromY]);
+        SetMap(fromX, fromY, 0);
+        isbooltaked = false;
+        if (!CutLines())
+        {
+            AddRandomBalls();
+            if (GameOver())
+                IsGameOver = true;
+        }
+
+    }
+
+    private bool GameOver()
+    {
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                if (map[i, j] == 0) return false;
+        return true;
+    }
+
+    private  bool[,] mark;
+    private bool CutLines()
+    {
+        int balls = 0;
+        mark = new bool[size, size];
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+            {
+                balls += CutLine(i, j, 1, 0);
+                balls += CutLine(i, j, 0, 1);
+                balls += CutLine(i, j, 1, 1);
+                balls += CutLine(i, j, -1, 1);
+            }
+        if (balls > 0)
+        {
+            Play_Cut();
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    if (mark[i, j]) SetMap(i, j, 0);
+            Record += 6 * balls;
+            return true;
+        }
+        return false;
+                
+    }
+
+    private int CutLine(int x0, int y0, int sx, int sy)
+    {
+        int ball = map[x0, y0];
+        if (ball == 0) return 0;
+        int count = 0;
+        for (int x = x0, y = y0; GetMap(x, y) == ball; x += sx, y += sy)
+        {
+            count++;
+        }
+        if (count < 5) return 0;
+        for (int x = x0, y = y0; GetMap(x, y) == ball; x += sx, y += sy)
+        {
+            mark[x, y] = true;
+        }
+        return count;
+    }
+
+    private int GetMap(int x, int y)
+    {
+        if (!OnMap(x,y)) return 0;
+        return map[x,y];
+    }
+
+    private bool OnMap(int x, int y)
+    {
+        return x >= 0 && x < size &&
+               y >= 0 && y < size;
+
+    }
+    private bool[,] used;
+    private bool CanMove(int tox, int toy)
+    {
+        used = new bool[size, size];
+        Walk(fromX, fromY, true);
+        return used[tox, toy]; 
+
+    }
+
+    private void Walk(int x, int y, bool start = false)
+    {
+        if (!start)
+        {
+            if (!OnMap(x, y)) return;
+            if (map[x, y] > 0) return;
+            if (used[x, y]) return;
+        }
+        used[x, y] = true;
+            Walk(x + 1, y);
+            Walk(x - 1, y);
+            Walk(x, y + 1);
+            Walk(x, y - 1);
+
+    }
+
+    private void TakeBall(int x, int y)
+    {
+        fromX = x;
+        fromY = y;
+        isbooltaked = true;
+    }
+}
